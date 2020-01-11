badf1ee8fc54263115083817974d00fd6d55fd62
zacum <mfilgyhty@hotmail.com>
2020-01-12 00:23:31 +0100

    Refactor account controller

--- src/main/java/com/moneytransfer/resources/AccountResource.java
+++ src/main/java/com/moneytransfer/resources/AccountResource.java
@@ -6,6 +6,7 @@ import com.google.inject.Inject;
 import com.moneytransfer.models.account.AccountCreateRequest;
 import com.moneytransfer.models.account.AccountResponse;
 import com.moneytransfer.services.AccountService;
+import spark.Request;
 
 import javax.money.UnknownCurrencyException;
 import java.util.Optional;
@@ -19,35 +20,45 @@ public class AccountResource {
 
     public void run() {
         post("/account", (request, response) -> {
-            AccountCreateRequest accountCreateRequest = null;
-            try {
-                accountCreateRequest = new Gson().fromJson(request.body(), AccountCreateRequest.class);
-            } catch (JsonSyntaxException e) {
-                e.printStackTrace();
-                halt(400, "Invalid JSON");
-            }
-            AccountResponse account = null;
-            try {
-                account = accountService.createAccount(accountCreateRequest);
-            } catch (UnknownCurrencyException e) {
-                halt(400, "Invalid currency code");
-            }
+            AccountCreateRequest accountCreateRequest = getAccountCreateRequest(request);
+            AccountResponse account = getAccountResponse(accountCreateRequest);
             return new Gson().toJson(account);
         });
-
         get("/account/:id", (request, response) -> {
-            Long accountId = null;
-            try {
-                accountId = Long.valueOf(request.params(":id"));
-            } catch (NumberFormatException nfe) {
-                halt(400, "Account id is not numerical");
-            }
+            Long accountId = getAccountId(request);
             Optional<AccountResponse> accountOpt = accountService.getAccount(accountId);
             if (accountOpt.isEmpty()) {
                 halt(404);
             }
             return new Gson().toJson(accountOpt.get());
         });
+    }
+
+    private AccountCreateRequest getAccountCreateRequest(Request request) {
+        try {
+            return new Gson().fromJson(request.body(), AccountCreateRequest.class);
+        } catch (JsonSyntaxException e) {
+            halt(400, "Invalid JSON");
+            throw e;
+        }
+    }
+
+    private AccountResponse getAccountResponse(AccountCreateRequest accountCreateRequest) {
+        try {
+            return accountService.createAccount(accountCreateRequest);
+        } catch (UnknownCurrencyException e) {
+            halt(400, "Invalid currency code");
+            throw e;
+        }
+    }
+
+    private Long getAccountId(Request request) {
+        try {
+            return Long.valueOf(request.params(":id"));
+        } catch (NumberFormatException e) {
+            halt(400, "Account id is not numerical");
+            throw e;
+        }
     }
 
 }
