2c0ba2e836d773497a6a0643ac244ae5a4383a40
zacum <mfilgyhty@hotmail.com>
2020-01-14 15:40:56 +0100

    Add Internal Server Error

--- src/main/java/com/moneytransfer/resources/DefaultResource.java
+++ src/main/java/com/moneytransfer/resources/DefaultResource.java
@@ -5,13 +5,13 @@ import com.moneytransfer.exceptions.IllegalTransactionException;
 
 import javax.money.UnknownCurrencyException;
 
-import static spark.Spark.before;
-import static spark.Spark.exception;
+import static spark.Spark.*;
 
 public class DefaultResource {
 
     public void run() {
         before(((request, response) -> response.type("application/json")));
+        internalServerError((request, response) -> "{\"message\":\"Internal Server Error\"}");
         exception(UnknownCurrencyException.class, (exception, request, response) -> {
             response.status(400);
             response.body("Invalid currency code");
